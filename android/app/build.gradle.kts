c9eec6c76d1546bbd8c57fd645505a475239e2b5
Tripti Patel <triptiverse@gmail.com>
2026-08-28 00:49:25 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
             useSupportLibrary = true
         }
 
-        buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8000/\"")
+        buildConfigField("String", "BASE_URL", "\"http://127.0.0.1:8000/\"")
     }
 
     buildTypes {
@@ -32,7 +32,7 @@ android {
             )
         }
         debug {
-            buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8000/\"")
+            buildConfigField("String", "BASE_URL", "\"http://127.0.0.1:8000/\"")
         }
     }
 
